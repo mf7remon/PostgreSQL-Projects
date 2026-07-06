4904784a64d30b7f62b02417bedb7f0b1895918e
mf7remon <rmf8395@gmail.com>
2026-07-06 20:40:31 +0600

    library_management

---
+++ Library Management System/library_management.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE library_management_system;
+
+CREATE TABLE students
+(
+    student_id SERIAL PRIMARY KEY,
+
+    full_name VARCHAR(100) NOT NULL,
+
+    roll INTEGER UNIQUE NOT NULL,
+
+    department VARCHAR(50) NOT NULL,
+
+    email VARCHAR(100) UNIQUE NOT NULL,
+
+    phone VARCHAR(20),
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
