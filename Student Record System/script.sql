bb1b1619c182f63a7490ef5340e4bb28a30eace0
mf7remon <rmf8395@gmail.com>
2026-07-06 09:57:50 +0600

    student_table

--- Student Record System/script.sql
+++ Student Record System/script.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE student_db;
 
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    roll INT UNIQUE NOT NULL,
+    department VARCHAR(50),
+    email VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
